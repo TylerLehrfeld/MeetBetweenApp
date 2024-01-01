eb34d7706da7c0508ee1475bc1e04defdd14af03
TylerLehrfeld <tylerlehrfeld@verizon.net>
2024-01-01 07:42:33 -0500

    changed mobile width of results

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -7,7 +7,7 @@ type LocType = {
 const Result = (locType: LocType) => {
     const { location } = locType;  
     return (
-        <div className="w-1/2 bg-gray-300 p-5 border-zinc-500 border-4 rounded">
+        <div className="w-5/6 lg:w-1/2 bg-gray-300 p-5 border-zinc-500 border-4 rounded">
             <div>
                 <div>
                     <p>{location.name}</p>
